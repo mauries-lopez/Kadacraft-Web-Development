d447a6738b5cbc62c96c274b24789bd6d390daf0
Mauries Lopez <maurieslopez1800@gmail.com>
2025-12-29 01:41:43 +0800

    progress commit
    - Members page (not clickable yet)
    - Mobile Menu

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer(){
     return(
         <>
             <footer className="w-full flex justify-center items-center bg-neutral-100 dark:bg-neutral-900 transition duration-300 p-5">
-                <p className="text-xs text-black dark:text-white italic">© 2025 KadaCraft. All rights reserved.</p>
+                <p className="text-xs text-black dark:text-white">© 2025 KadaCraft. All rights reserved.</p>
             </footer>
         </>
     )
