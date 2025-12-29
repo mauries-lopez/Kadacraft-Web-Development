dfa2bb19844d8228e03bc06a6b472d2e6c640f4c
Mauries Lopez <maurieslopez1800@gmail.com>
2025-12-29 13:21:46 +0800

    quick fix
    width issue on FAQ section

--- components/landing/About.tsx
+++ components/landing/About.tsx
@@ -4,7 +4,7 @@ export default function About(){
     return (
         <>
             <section className="w-full flex flex-col md:flex-row bg-neutral-100 dark:bg-neutral-900 ">
-                <div className="p-10 md:p-20 transition duration-300 flex flex-col justify-center items-center">
+                <div className="w-full p-10 md:p-20 transition duration-300 flex flex-col justify-center items-center">
                     {/* Foreground content */}
                     <div className="flex flex-col justify-center items-center gap-y-3">
                         <div className="w-full flex justify-center items-center ">
@@ -27,7 +27,7 @@ export default function About(){
 
                 <div className="w-full flex flex-col justify-center items-center p-10 md:p-20 gap-y-3">
                     <img src="/image/faqs.png" className="w-full max-w-50 md:max-w-100 h-8 md:h-10 object-contain" />
-                    <div className="w-full flex flex-col gap-y-2">
+                    <div className="flex flex-col gap-y-2">
                         <Faq question="Paano makasali ng KadaCraft?" desc="Walang kasiguraduhan na makakasali ka ng KadaCraft, pero ito ang iilan sa mga pamantayan namin sa pagpili. (1) At least 18 years old. (2) Content Creator/Streamer on any platform. (3) Creates Minecraft Content. (4) Dedicated & Consistent. (5) Minecraft Java Edition"/>
                         <Faq question="Bakit wala ung ibang KadaCraft Members?" desc="Iba-iba ang dahilan. Maaaring 'busy' sa tunay na buhay at nagtra-trabaho. Kapag wala ang 'link' nila dito, ibig sabihin 'di na sila kabilang sa KadaCraft. Sa anong dahilan? marami, maaaring personal at 'di na dapat tanungin. Maaaring 'di talaga nila gusto mag-YouTube, dahil walang kasiguraduhan ang YouTube bilang trabaho."/>
                         <Faq question="Anong mga mods ng KadaCraft?" desc="Wala masyado. Pinapanatili naming 'vanilla' ang halos lahat ng aming mga season. Kung maglalagay kami ng maraming mods, mag-iiba na ang tawag dito, tulad nang ginawa namin sa Kada Z. Kadalasan, ang nakikita ninyo sa KadaCraft 7 ay mga resource packs at data packs. Karamihan sa mga mods namin ay server-side at hindi naman magagamit sa singleplayer."/>
