5602b226553702cdaa8447355063358c0c0bbd2f
Mauries Lopez <maurieslopez1800@gmail.com>
2025-12-29 13:21:46 +0800

    quick fix
    width issue on FAQ section

--- components/landing/Faq/Faq.tsx
+++ components/landing/Faq/Faq.tsx
@@ -13,7 +13,7 @@ export default function Faq({question, desc}: FaqProps){
 
     return (
         <>
-            <div className="w-full text-center bg-white dark:bg-neutral-800 p-2 drop-shadow-md rounded-full border-2 border-yellow-400 cursor-pointer" onClick={() => setViewDesc(!viewDesc)}>
+            <div className="w-full text-center bg-white dark:bg-neutral-800 py-2 px-4 drop-shadow-md rounded-full border-2 border-yellow-400 cursor-pointer" onClick={() => setViewDesc(!viewDesc)}>
                 <p className="text-black dark:text-white text-sm md:text-base transition duration-300 ">{question}</p>
             </div>
             {viewDesc && (
