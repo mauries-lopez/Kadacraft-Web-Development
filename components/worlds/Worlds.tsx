871dae6620cbcd11b70a4080abb6f3eb7469d92b
Mauries Lopez <maurieslopez1800@gmail.com>
2026-01-08 10:13:56 +0800

    Add Seasons 5 and 6 worlds and galleries
    Added images and gallery data for KadaCraft Seasons 5 and 6. Updated Worlds page to include new seasons and download links. Added 'Mods' link to navigation bars. Removed unused FAQ and deleted legacy JavaScript files from public/js.

--- components/worlds/Worlds.tsx
+++ components/worlds/Worlds.tsx
@@ -32,6 +32,14 @@ export default function Worlds(){
             season: "Season 4.1",
             gallery: ["/image/season4.1/kadacraftS4.1_1.png", "/image/season4.1/kadacraftS4.1_2.png", "/image/season4.1/kadacraftS4.1_3.png", "/image/season4.1/kadacraftS4.1_4.png", "/image/season4.1/kadacraftS4.1_5.png", "/image/season4.1/kadacraftS4.1_6.png", "/image/season4.1/kadacraftS4.1-best.png"]
         },
+        {
+            season: "Season 5",
+            gallery: ["/image/season5/kadacraftS5_1.png", "/image/season5/kadacraftS5_2.png", "/image/season5/kadacraftS5_3.png", "/image/season5/kadacraftS5_4.png", "/image/season5/kadacraftS5_5.png", "/image/season5/kadacraftS5_6.png", "/image/season5/kadacraftS5-best.png"]
+        },
+        {
+            season: "Season 6",
+            gallery: ["/image/season6/kadacraftS6_1.png", "/image/season6/kadacraftS6_2.png", "/image/season6/kadacraftS6_3.png", "/image/season6/kadacraftS6_4.png", "/image/season6/kadacraftS6_5.png", "/image/season6/kadacraftS6_6.png", "/image/season6/kadacraftS6-best.png"]
+        }
     ]
 
     const [seasonName, setSeasonName] = useState("undefined");
@@ -72,21 +80,30 @@ export default function Worlds(){
 
     return(
         <>
-            <div className="w-full h-svh flex flex-col justify-center items-center bg-neutral-100 ">
-                <Background srcLight="/image/temp-background_light.png" srcDark="/image/temp-background_dark.png "/>
+            <div className="relative w-full min-h-svh md:p-10 overflow-hidden flex flex-col justify-center items-center">
+                {/* Background layer */}
+                <div className="absolute inset-0">
+                    <Background
+                        srcLight="/image/temp-background_light.png"
+                        srcDark="/image/temp-background_dark.png "
+                    />
+                </div>
+                
                 <div className="w-full h-130 md:h-full flex flex-row md:grid md:grid-cols-2 lg:grid-cols-3 justify-start items-center gap-10 mt-10 p-10 md:p-20 overflow-x-scroll z-10 snap-x snap-mandatory md:overflow-hidden slide-in-top">
                     <World title="Season 1" href="https://www.mediafire.com/file/s2b7j3ckj9dkgvx/KadaCraft_S1.mcworld/file" srcImage="/image/season1/kadacraftS1-best.png" edition="bedrock" setSeasonName={setSeasonName} setIsViewGalleryBool={setIsViewGalleryBool}/>
                     <World title="Season 2" href="https://www.mediafire.com/file/d6akwu2lvm2xz6f/KadaCraft_Season_2.mcworld/file" srcImage="/image/season2/kadacraftS2-best.png" edition="bedrock" setSeasonName={setSeasonName} setIsViewGalleryBool={setIsViewGalleryBool}/>
                     <World title="Season 3" href="https://www.mediafire.com/file/rdnv4o2enbjabrh/KadaCraft_Season_3_%2528Pruned_Java_World%2529.rar/file" srcImage="/image/season3/kadacraftS3-best.png" edition="java" setSeasonName={setSeasonName} setIsViewGalleryBool={setIsViewGalleryBool}/>
                     <World title="Season 4" href="https://www.mediafire.com/file/9u1k34wta8a3z14/KadaCraft_Season_Four.rar/file" srcImage="/image/season4/kadacraftS4-best.png" edition="java" setSeasonName={setSeasonName} setIsViewGalleryBool={setIsViewGalleryBool}/>
                     <World title="Season 4.1" href="https://drive.google.com/file/d/1UuIYC5o4mUoje6wnNLpfUD1s9MOM0r64/view" srcImage="/image/season4.1/kadacraftS4.1-best.png" edition="java" setSeasonName={setSeasonName} setIsViewGalleryBool={setIsViewGalleryBool}/>
+                    <World title="Season 5" href="https://drive.google.com/file/d/1ygwXV1d_y9Aq3ErrZhYVOt6DbKg1c_1i/view" srcImage="/image/season5/kadacraftS5-best.png" edition="java" setSeasonName={setSeasonName} setIsViewGalleryBool={setIsViewGalleryBool}/>
+                    <World title="Season 6" href="https://drive.google.com/file/d/1XDviVUyZ1VZ2TiGEq0AqDITL5lJvyf4b/view" srcImage="/image/season6/kadacraftS6-best.png" edition="java" setSeasonName={setSeasonName} setIsViewGalleryBool={setIsViewGalleryBool}/>
                 </div>
             </div>
 
             {/* Pictures */}
-            <div className={`${viewGalleryBool ? "flex " : "hidden"} absolute inset-0 justify-center items-center z-50`}>
+            <div className={`${viewGalleryBool ? "flex " : "hidden"} fixed inset-0 justify-center items-center z-50 overflow-auto`}>
                 <div
-                    className="w-full h-svh flex items-center justify-center bg-neutral-700/80 px-10 md:px-70"
+                    className="w-full min-h-screen flex items-center justify-center bg-neutral-700/80 px-10 md:px-70"
                     onClick={() => endViewGallery()}
                 >
                     <div onClick={e => e.stopPropagation()} className="w-full flex flex-col slide-in-top justify-center">
